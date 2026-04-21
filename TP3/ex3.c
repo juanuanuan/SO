9200f8077d497244cf6f22ce912cfb701052a1e2
João Pontes <A11657@alunos.uminho.pt>
2026-04-21 14:43:38 +0100

    dummy push

--- TP3/ex3.c
+++ TP3/ex3.c
@@ -5,14 +5,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 
-int main(char* command, char **argv){
+int main(int argc, char **argv){
     pid_t pid; 
     int status;
+
+    if(argc < 2){
+        fprintf(stderr, "%s <command>\n", argv[0]);
+        return 1;
+    }
 
     if((pid = fork()) < 0) return -1;
 
     if(pid == 0){
+         execl("/bin/sh", "sh", "-c", argv[1], NULL); // filho executa o bash
+         fprintf(stderr, "execlp failed: %s\n", strerror(errno)); // em caso de falha, dá erro, com errno ("error number")
+         exit (1); // saiu com código 1
 
-    }
+    } else {
+        wait(&status); // o processo pai espera pelo fim da execução do bash
+        if(WIFEXITED(status)){
+            fprintf(stdout, "Precess exited with code: %d\n", WEXITSTATUS(status));
+        }
+    } return 0;
 }
---
+++ TP3/ex4.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
+int main (int argc, char **argv){
+    
+}
